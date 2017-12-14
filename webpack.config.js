6633f5d86eb66c049c9d34a5163b59f4a4e3628f
LongLD <longdd21081994@gmail.com>
2017-12-14 14:00:57 +0700

    none

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ var path = require('path');
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: {
-        pokemon:'./PokemonApp/index.js'
+        //pokemon:'./PokemonApp/index.js'
+        jobmanager: './JobManager/index.js'
 
     },
     output: {
-        path: path.resolve(__dirname, 'dist') + '/pokemonapp',
+        path: path.resolve(__dirname, 'dist') + '/jobmanager', //'/pokemonapp',
         filename: '[name].bundle.js',
-        publicPath: '/pokemonapp/'
+        publicPath: '/jobmanager'
+        //publicPath: '/pokemonapp/'
     },
     module: {
         loaders: [
@@ -26,9 +28,13 @@ module.exports = {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
             },
-            { test    : /\.(?:png|jpg|jpeg)$/,
-                include : path.join(__dirname, 'img'),
-                loader  : 'url-loader?limit=30000&name=images/[name].[ext]'}
+            {
+                test: /\.(?:png|jpg|jpeg)$/,
+                include: path.join(__dirname, 'img'),
+                loader: 'url-loader?limit=30000&name=images/[name].[ext]'
+            }
         ]
-    }
+    },
+    devtool: '#eval-inline-source-map',
+    
 };
